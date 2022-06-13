e9dc3de070516457cbb8a6d0946bb0815f2ddb4c
Bruno Reinaldo <brunoreinaldo1998@gmail.com>
2022-06-13 20:50:25 -0300

    gitignore

--- PRONTU/PRONTU/Connection.cs
+++ PRONTU/PRONTU/Connection.cs
@@ -31,33 +31,5 @@ namespace PRONTU
         {
             conn.Close ();
         }
-
-        /*public static void Main()
-        {
-            string connStr = "server=localhost;user=root;database=prontu;port=3306;password=toor";
-            MySqlConnection conn = new MySqlConnection(connStr);
-            try
-            {
-                Console.WriteLine("Connecting to MySQL...");
-                conn.Open();
-
-                string sql = "SELECT COUNT(*) FROM Country";
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-                object result = cmd.ExecuteScalar();
-                if (result != null)
-                {
-                    int r = Convert.ToInt32(result);
-                    Console.WriteLine("Number of countries in the world database is: " + r);
-                }
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-
-            conn.Close();
-            Console.WriteLine("Done.");
-        }*/
     }
 }
