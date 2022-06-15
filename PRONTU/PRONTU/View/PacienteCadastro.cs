2726d6a1c7ab7c3d9aac5fba3cbe3e3996012118
MarksChiele <marcos.vcd@outlook.com>
2022-06-14 23:25:04 -0300

    Design das telas
    Design das telas

--- PRONTU/PRONTU/View/PacienteCadastro.cs
+++ PRONTU/PRONTU/View/PacienteCadastro.cs
@@ -16,5 +16,165 @@ namespace PRONTU.View
         {
             InitializeComponent();
         }
+
+        private void PacienteCadastro_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void respTextField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ConvenioTextField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NascimentoTextField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CpfRespTextField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumeroTextField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TelefoneTextField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EmailTextField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RuaTextField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumRuaTextField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CompelmentoTextField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BairroTextField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CidadeTextField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EstadoTextField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ObsTextField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotaoCancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotaoSalvar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotaoBuscar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotaoEditar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotaoExcluir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotaoIncluir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotaoHistorico_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotaoAgenda_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
